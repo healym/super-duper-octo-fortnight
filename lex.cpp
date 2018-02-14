47ac1889db14b9e48f930b0658c34b8f8f7b7e2c
Matthew Healy <mhealy@embyrtech.io>
2018-02-14 12:30:36 -0600

    add all check functions

--- lex.cpp
+++ lex.cpp
@@ -3,53 +3,210 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     return 0;
 }
 
-bool is_int(string str)
-{
-  char c;
+bool IsInt(string str) {
+  int k;
   int state = 0;
-  for(int i=0; i<str.length(); i++)
-  {
-    c = str[i];
-    switch(c)
-    {
-      case '+':
-      case '-':
-      if(state == 0)
-      {
-        state = 1;
-      }
-      else
-      {
-        return false;
-      }
-        break;
-      case '9':
-      case '8':
-      case '7':
-      case '6':
-      case '5':
-      case '4':
-      case '3':
-      case '2':
-      case '1':
-      case '0':
+  for (int i = 0; i < str.length(); i++) {
+    k = int(str[i]);
+    if (k == int('+') || k == int('-')) {
+        if (state == 0) {
+          state = 1;
+        } else {
+          return false;
+        }
+    } else if (k < 58 && k >= 48) {
         state = 2;
-        break;
-      default:
+    } else {
         return false;
     }
   }
-  if(state == 2)
-  {
-    return true;
+  return state == 2;
+}
+
+bool IsDec(string str) {
+  int k;
+  int state = 0;
+  for (int i = 0; i < str.length(); i++) {
+    k = int(str[i]);
+    if (k == int('+') || k == int('-')) {
+        if (state == 0) {
+          state = 1;
+        } else {
+          return false;
+        }
+    } else if (k < 58 && k >= 48) {
+        if (state == 0 || state == 1) {
+          state = 2;
+        } else if (state == 2) {
+          state = 2;
+        } else if (state == 3 || state == 4) {
+          state = 4;
+        }
+    } else if (k == int('.')) {
+        if (state == 2) {
+          state = 3;
+        } else {
+          return false;
+        }
+    } else {
+        return false;
+    }
   }
-  else
-  {
-    return false;
+  return state == 4;
+}
+
+bool IsSci(string str) {
+  int state = 0;
+  int k;
+  for(int i = 0; i < str.length(); i++) {
+    k = int(str[i]);
+    if (k == int('+') || k == int('-')) {
+      if (state == 0) {
+        state = 1;
+      } else if (state == 4) {
+        state = 5;
+      } else {
+        return false;
+      }
+    } else if (k < 58 && k >= 48) {
+        if (state == 0 || state == 1) {
+          state = 2;
+        } else if (state == 2) {
+          state = 2;
+        } else if (state == 3) {
+          state = 3;
+        } else if (state == 4 || state == 5 || state == 6) {
+          state = 6;
+        } else {
+          return false;
+        }
+    } else if (k == int('.')) {
+        if (state == 2) {
+          state = 3;
+        } else {
+          return false;
+        }
+    } else if (k == int('E') || k == int('e')) {
+        if (state == 3) {
+          state = 4;
+        } else {
+          return false;
+        }
+    } else {
+      return false;
+    }
   }
+  return state == 6;
+}
+
+bool IsHex(string str) {
+  int state = 0;
+  int k;
+  for (int i = 0; i < str.length(); i++) {
+    k = int(str[i]);
+    if (k < 58 && k >= 48 || k < 91 && k > 65) {
+      if (state == 0 || state == 1) {
+        state = 1;
+      } else {
+        return false;
+      }
+    } else if (k == int('H')) {
+      if (state == 1) {
+        state = 2;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  return state == 2;
+}
+
+bool IsId(string str) {
+  int state = 0;
+  int k;
+  for (int i = 0; i < str.length(); i++) {
+    k = int(str[i]);
+    if (k < 91 && k >=65 || k < 123 && k >= 97){
+      if (state == 0) {
+        state = 1;
+      } else if (state == 1) {
+        state = 1;
+      }
+    } else if (k<58 && k>=48) {
+      if (state == 1) {
+        state = 1;
+      } else {
+        return false;
+      }
+    } else if (k == int('_')) {
+      if (state = 1) {
+        state = 1;
+      } else {
+        return false;
+      }
+    }
+  }
+  return state == 1;
+}
+
+bool IsKeyword(string str) {
+  return str == "WHILE" || str == "IF" || str == "ELSE" || str == "PRINT";
+}
+
+bool IsPhoneNumber(string str) {
+  int state = 0;
+  int k;
+  for (int i = 0; i < str.length(); i++) {
+    k = int(str[i]);
+    if (k < 58 && k >= 48) {
+      if (state > 0 && state < 3
+      || state > 4 && state < 8
+      || state > 8 && state < 13
+      || state > 13 && state < 16
+      || state > 17 && state < 20) {
+        state++;
+      } else if (state == 0) {
+        state = 14;
+      } else {
+        return false;
+      }
+    } else if (k == int('.')) {
+      if (state == 16) {
+        state = 17;
+      } else if (state == 20) {
+        state = 9;
+      } else {
+        return false;
+      }
+    } else if (k == int('-')) {
+      if (state == 8) {
+        state = 9;
+      } else if (state == 16) {
+        state = 5;
+      } else {
+        return false;
+      }
+    } else if (k == int('(')) {
+      if (state == 0) {
+        state = 1;
+      } else {
+        return false;
+      }
+    } else if (k == int(')')) {
+      if (state == 5) {
+        state = 6;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  return state == 13;
 }
